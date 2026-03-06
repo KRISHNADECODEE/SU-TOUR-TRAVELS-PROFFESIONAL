1098e811a0370169c383bae623b7d4e27058a492
KRISHNADECODEE <ks2537319@gmail.com>
2026-03-06 09:35:50 +0530

    Bump Service Worker to v2 to force cache refresh and show Explore buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'su-tour-cache-v1';
+const CACHE_NAME = 'su-tour-cache-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -14,6 +14,20 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
